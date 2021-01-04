88cf668b37e1267ddb989a5b6f7e50241ec207d7
Taras Khrystenko <tmkhrystenko@gmail.com>
2021-01-04 15:50:33 +0100

    v1.0
    - added about page
    - added express dependency

---
+++ src/components/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1 class="heading">About</h1>
+  <div class="about-container">
+    <p class="about-text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan
+      auctor interdum. Sed luctus nibh justo, et hendrerit risus eleifend in.
+      Nunc tincidunt neque non sem accumsan porttitor. Quisque mollis, tortor
+      congue feugiat vulputate, felis sapien venenatis arcu, at varius velit
+      lacus et sapien. Sed pulvinar id nulla ac interdum. Quisque gravida auctor
+      risus et ullamcorper. Aliquam erat volutpat. Morbi placerat vestibulum
+      est, at scelerisque ante gravida eget. Pellentesque consequat lacus ac
+      justo posuere, sit amet aliquet dui fermentum. Pellentesque habitant morbi
+      tristique senectus et netus et malesuada fames ac turpis egestas. Sed
+      vitae massa a magna efficitur pharetra quis et nisl. Ut ultricies molestie
+      malesuada. Phasellus volutpat mattis tellus, ut posuere neque molestie a.
+      Cras blandit vitae est ac pellentesque. Donec tincidunt consequat turpis,
+      id placerat eros iaculis ut. Cras vehicula diam sed molestie viverra.
+      Donec imperdiet suscipit eros vitae gravida. Proin augue tortor, vulputate
+      lacinia nunc id, luctus sodales enim. Ut finibus turpis ac nisl
+      condimentum consequat. Nulla quis luctus nulla. Sed sed augue id magna
+      luctus suscipit. Sed pretium vel nisl ut dapibus. Proin eleifend sed erat
+      eget malesuada.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  // ...
+};
+</script>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Dosis:wght@500&family=Open+Sans:wght@600&display=swap");
+
+.heading {
+  background-color: black;
+  color: white;
+  padding: 3rem;
+}
+
+.about-container {
+  padding: 1rem 3rem;
+  width: 100%;
+
+  .about-text {
+    line-height: 2rem;
+    font-size: 1.4rem;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import JobList from "@/components/jobs/JobList.vue";
 import PostJob from "@/components/posts/PostJob.vue";
 import ManageView from "@/components/manage/ManageView.vue";
+import About from "@/components/About.vue";
 
 // 2. Define some routes.
 const routes = [
   { path: "/", component: JobList },
   { path: "/post-a-job", component: PostJob },
-  { path: "/manage-jobs", component: ManageView }
+  { path: "/manage-jobs", component: ManageView },
+  { path: "/about", component: About }
 ];
 
 // 3. Create the router instance and pass the `routes` option.
